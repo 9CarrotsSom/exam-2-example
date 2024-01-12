215478f0fc9078d1116ec12e037fc2b3e4b4f294
♻ NOr._Nor ♻ <79706975+ImJustNon@users.noreply.github.com>
2024-01-12 09:45:33 +0700

    Add files via upload

---
+++ new/1/1_typescript.ts
@@ -0,0 +1,8 @@
+
+
+for(let i: number = 1; i <= 50; i++ ){
+    console.log(`===================== เเม่ ${String(i)} ======================`);
+    for(let ii: number = 1; ii <= 12; ii++){
+        console.log(`${String(i)} X ${String(ii)} = ${String(i * ii)}`);
+    }
+}
